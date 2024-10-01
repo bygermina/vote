aa8bacbdd35fc75ca85a60ed230e5fca0e28c103
Xenia.Liubachka <bygermina@gmail.com>
2024-10-01 21:05:55 +0200

    fix

--- 2_form/server.js
+++ 2_form/server.js
@@ -8,7 +8,7 @@ const webserver = express();
 webserver.use(express.json()); 
 
 const PORT = process.env.NODE_ENV === "development" ? 3050 : 7780;
-const IP = process.env.NODE_ENV === "development" ? "http://localhost" : "https://178.172.195.18";
+const IP = process.env.NODE_ENV === "development" ? "http://localhost" : "http://178.172.195.18";
 const ORIGIN = `${IP}:${PORT}`;
 
 webserver.listen(PORT, () => { 
